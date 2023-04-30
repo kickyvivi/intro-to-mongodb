52ea094e6fe003d2ee5906bbec707aa63e89a78f
Vivek N R <vivi94458@gmail.com>
2023-04-30 20:24:53 +0530

    [Source]Demo mflix project

---
+++ mflix/mflix 2/env.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=mflix/mflix.py
+set FLASK_DEBUG=false
+set MFLIX_DB_URI="mongodb+srv://analytics:<password>@mflix.n69ze95.mongodb.net/test" & REM Replace XXXX with your MongoDB Connection URI
